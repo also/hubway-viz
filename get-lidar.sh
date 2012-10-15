b14c37501f439b05a2e046030adb8e875342dc49
Ryan Berdeen <ryan@ryanberdeen.com>
2012-10-14 23:04:33 -0400

    deal with lidar data

---
+++ get-lidar.sh
@@ -0,0 +1,14 @@
+mkdir -p data/lidar
+cd data/lidar
+
+237894
+lidar_files="233898 233902 237898 237902 241898 241902 241894 237894 233894"
+for file in $lidar_files
+do
+  echo getting $file
+  curl -O http://wsgw.mass.gov/data/gispub/lidar/2002_Boston_area/dem_tif/bare$file.tif.bz2
+done
+
+echo decompressing
+bunzip2 *.bz2
+cd
---
+++ process-lidar.sh
@@ -0,0 +1,11 @@
+echo merging images
+PYTHONPATH=/usr/local/Cellar/gdal/1.9.2/lib/python2.7/site-packages /usr/local/bin/gdal_merge.py data/lidar/*.tif -o data/lidar.tif
+
+echo reprojecting
+gdalwarp -s_srs EPSG:26986 -t_srs EPSG:3785 -r bilinear data/lidar.tif data/lidar-3785.tif
+
+echo generating hillshade
+gdaldem hillshade -co compress=lzw data/lidar-3785.tif data/lidar-hillshade-3785.tif
+
+echo generating slope
+gdaldem slope data/lidar-3785.tif data/lidar-slope-3785.tif
