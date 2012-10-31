ba6dc48978fecec7f685f707ed6dc787af671174
Ryan Berdeen <ryan@ryanberdeen.com>
2012-10-31 14:36:04 -0400

    fix lidar projection

--- process-lidar.sh
+++ process-lidar.sh
@@ -2,7 +2,7 @@ echo merging images
 PYTHONPATH=/usr/local/Cellar/gdal/1.9.2/lib/python2.7/site-packages /usr/local/bin/gdal_merge.py data/lidar/*.tif -o data/lidar.tif
 
 echo reprojecting
-gdalwarp -s_srs EPSG:26986 -t_srs EPSG:3785 -r bilinear data/lidar.tif data/lidar-3785.tif
+gdalwarp -t_srs EPSG:3857 -r bilinear data/lidar.tif data/lidar-3785.tif
 
 echo coloring
 gdaldem color-relief data/lidar-3785.tif ramp.txt data/lidar-colored-3785.tif
